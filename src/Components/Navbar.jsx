e75985df6f9843555444e5d4702cdacf9f8ec57c
Syed Saeed Bukhari <saeed.workspace.sqa@gmail.com>
2026-03-17 19:08:12 +0500

    add conf

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Logo from "../assets/logo.png";
+import Logo from "../assets/Logo.png";
 import { Menu, X } from "lucide-react";
 import ResponsiveMenu from "./ResponsiveMenu";
 
